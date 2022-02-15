e63400f8107d0d6ee4695c5e054f38b51702d7b1
sandeep.maregowda@gmail.com <sandeep.maregowda@gmail.com>
2022-02-15 22:49:54 +0000

    commit after jinja basics

---
+++ models/marts/core/order_summary.sql
@@ -0,0 +1,22 @@
+{%- set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] -%}
+with payments as
+(
+    select * from {{ ref('stg_payments') }}
+),
+final as
+(
+    select
+        order_id,
+        {% for payment_method in payment_methods %}
+            sum(case when paymentmethod = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount
+            {%- if not loop.last -%}
+            ,
+            {%- endif -%}
+        {% endfor %}
+    from
+        payments
+    where
+        status = 'success'
+    group by 1
+)
+Select * from final
